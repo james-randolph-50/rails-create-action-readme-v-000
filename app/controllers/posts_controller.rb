d49367ed9d6736dec803a173586085e3f476d63c
James Randolph <james.randolph2016@gmail.com>
2018-07-10 17:21:01 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,11 +3,12 @@ class PostsController < ApplicationController
 		@posts = Post.all
 	end
 	
-	def create
-  post = Post.new
-  post.title = params[:title]
-  post.description = params[:description]
-  post.save
+def create
+  @post = Post.new
+  @post.title = params[:title]
+  @post.description = params[:description]
+  @post.save
+  redirect_to post_path(@post)
 end
 
 	def show
