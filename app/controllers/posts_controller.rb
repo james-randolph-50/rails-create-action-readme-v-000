771299e14421dd127bcd6248fcce7dce6f76fe83
James Randolph <james.randolph2016@gmail.com>
2018-07-10 17:20:42 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,13 @@ class PostsController < ApplicationController
 	def index
 		@posts = Post.all
 	end
+	
+	def create
+  post = Post.new
+  post.title = params[:title]
+  post.description = params[:description]
+  post.save
+end
 
 	def show
 		@post = Post.find(params[:id])
